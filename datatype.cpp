d68bdce3a46e12a5319dbca08f0d4aeb8fa206a9
Abuzar Haider <abuzarsarwary3@gmail.com>
2021-06-14 04:48:23 +0500

    My first commit of data types

---
+++ datatype.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    cin >> i >> l >> c >> f >> d;
+    cout << i << '\n' << l << '\n' << c << '\n';
+    cout << fixed << setprecision(3) << f << '\n';
+    cout << fixed << setprecision(9) << d << '\n';
+    
+    return 0;
+}
